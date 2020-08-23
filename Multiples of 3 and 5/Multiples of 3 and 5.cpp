7e90c60402b0cf52214a0e65a34a08321815c795
เอกภพ ขาวนวล < @DESKTOP-SURU41J>
2020-08-23 15:50:48 +0700

    Finish

--- Multiples of 3 and 5/Multiples of 3 and 5.cpp
+++ Multiples of 3 and 5/Multiples of 3 and 5.cpp
@@ -3,6 +3,14 @@ int main()
 {
 	int n[1000];
 	int ans = 0;
+	char name[50];
+	printf("Input your name : ");
+	gets_s(name);
+	printf("Welcome %s\n", name);
+	printf("The question is \n");
+	printf("If we list all the natural numbers below 10 that are multiples of 3 or 5\n");
+	printf("we get 3, 5, 6 and 9. The sum of these multiples is 23.\n");
+	printf("Find the sum of all the multiples of 3 or 5 below 1000.");
 	for (int i = 0; i < 1000; i++)
 	{
 		if (i%3 == 0 || i%5 == 0)
@@ -15,6 +23,6 @@ int main()
 		}
 		ans += n[i];
 	}
-	printf("Result = %d", ans);
+	printf("\n\nResult = %d", ans);
 	return 0;
 }
